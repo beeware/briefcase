4e79ae0fed2fa6056a3a640f6ee4609bacfce4a8
Russell Keith-Magee <russell@keith-magee.com>
2018-07-29 13:07:36 +0800

    Formally add support for recent Python versions.

--- setup.py
+++ setup.py
@@ -58,6 +58,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Software Development',
         'Topic :: Utilities',
