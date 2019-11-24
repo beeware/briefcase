67558b9c109e78f07ba77f52cc5752c39349da05
Russell Keith-Magee <russell@keith-magee.com>
2019-11-24 16:40:45 +0800

    Allow for extra arguments on windows backend.

--- src/briefcase/platforms/windows/__init__.py
+++ src/briefcase/platforms/windows/__init__.py
@@ -4,7 +4,7 @@ DEFAULT_OUTPUT_FORMAT = 'msi'
 
 class WindowsMixin:
     def __init__(self, *args, output_format, **kwargs):
-        super().__init__(platform='windows', output_format=output_format)
+        super().__init__(*args, platform='windows', output_format=output_format, **kwargs)
 
     def verify_tools(self):
         pass
