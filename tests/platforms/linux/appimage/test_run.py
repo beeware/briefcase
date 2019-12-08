b76886cc17715db15105ef8b032b6a87d827c645
Russell Keith-Magee <russell@keith-magee.com>
2019-12-08 17:23:38 +0800

    Made Linux AppImage run tests resilient to test architecture.

--- tests/platforms/linux/appimage/test_run.py
+++ tests/platforms/linux/appimage/test_run.py
@@ -9,12 +9,16 @@ from briefcase.platforms.linux.appimage import LinuxAppImageRunCommand
 def test_run_app(first_app_config, tmp_path):
     "A linux App can be started"
     command = LinuxAppImageRunCommand(base_path=tmp_path)
+
+    # Set the host architecture for test purposes.
+    command.host_arch = 'wonky'
+
     command.subprocess = mock.MagicMock()
 
     command.run_app(first_app_config)
 
     command.subprocess.run.assert_called_with(
-        [str(tmp_path / 'linux' / 'First App-0.0.1-x86_64.AppImage')],
+        [str(tmp_path / 'linux' / 'First App-0.0.1-wonky.AppImage')],
         check=True
     )
 
@@ -22,6 +26,10 @@ def test_run_app(first_app_config, tmp_path):
 def test_run_app_failed(first_app_config, tmp_path):
     "If there's a problem started the app, an exception is raised"
     command = LinuxAppImageRunCommand(base_path=tmp_path)
+
+    # Set the host architecture for test purposes.
+    command.host_arch = 'wonky'
+
     command.subprocess = mock.MagicMock()
     command.subprocess.run.side_effect = BriefcaseCommandError('problem')
 
@@ -30,6 +38,6 @@ def test_run_app_failed(first_app_config, tmp_path):
 
     # The run command was still invoked, though
     command.subprocess.run.assert_called_with(
-        [str(tmp_path / 'linux' / 'First App-0.0.1-x86_64.AppImage')],
+        [str(tmp_path / 'linux' / 'First App-0.0.1-wonky.AppImage')],
         check=True
     )
