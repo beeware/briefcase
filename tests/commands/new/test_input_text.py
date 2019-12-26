964f1f6157b1764ce5945b3663ea8619740f1aab
Russell Keith-Magee <russell@keith-magee.com>
2019-12-26 08:24:51 +0800

    Added more tests of prompt capitalization.

--- tests/commands/new/test_input_text.py
+++ tests/commands/new/test_input_text.py
@@ -45,3 +45,16 @@ def test_input_with_default(new_command):
     assert new_command.input.call_count == 1
     new_command.input.assert_called_with("My Variable [goodbye]: ")
     assert value == "goodbye"
+
+
+def test_prompt_capitalization(new_command):
+    "The prompt is capitalized appropriately"
+    new_command.input = mock.MagicMock(return_value='hello')
+
+    new_command.input_text(
+        intro="Some introduction",
+        variable="user's URL",
+        default="goodbye"
+    )
+
+    new_command.input.assert_called_with("User's URL [goodbye]: ")
