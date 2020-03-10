36329389cf84805439f2f4ab3a8291a90980915e
Sagi Shadur <saroad2@gmail.com>
2020-03-10 18:20:00 +0200

    run black on dev command's tests

--- tests/commands/dev/conftest.py
+++ tests/commands/dev/conftest.py
@@ -15,16 +15,16 @@ def dev_command(tmp_path):
 @pytest.fixture
 def first_app_uninstalled(tmp_path):
     # Make sure the source code exists
-    (tmp_path / 'src' / 'first').mkdir(parents=True, exist_ok=True)
-    with (tmp_path / 'src' / 'first' / '__init__.py').open('w') as f:
+    (tmp_path / "src" / "first").mkdir(parents=True, exist_ok=True)
+    with (tmp_path / "src" / "first" / "__init__.py").open("w") as f:
         f.write('print("Hello world")')
 
     return AppConfig(
-        app_name='first',
-        bundle='com.example',
-        version='0.0.1',
-        description='The first simple app',
-        sources=['src/first'],
+        app_name="first",
+        bundle="com.example",
+        version="0.0.1",
+        description="The first simple app",
+        sources=["src/first"],
     )
 
 
@@ -32,43 +32,43 @@ def first_app_uninstalled(tmp_path):
 def first_app(tmp_path, first_app_uninstalled):
     # The same fixture as first_app_uninstalled,
     # but ensures that the .dist-info folder for the app exists
-    (tmp_path / 'src' / 'first.dist-info').mkdir(exist_ok=True)
+    (tmp_path / "src" / "first.dist-info").mkdir(exist_ok=True)
     return first_app_uninstalled
 
 
 @pytest.fixture
 def second_app(tmp_path):
     # Make sure the source code exists
-    (tmp_path / 'src' / 'second').mkdir(parents=True, exist_ok=True)
-    with (tmp_path / 'src' / 'second' / '__init__.py').open('w') as f:
+    (tmp_path / "src" / "second").mkdir(parents=True, exist_ok=True)
+    with (tmp_path / "src" / "second" / "__init__.py").open("w") as f:
         f.write('print("Hello world")')
 
     # Create the dist-info folder
-    (tmp_path / 'src' / 'second.dist-info').mkdir(exist_ok=True)
+    (tmp_path / "src" / "second.dist-info").mkdir(exist_ok=True)
 
     return AppConfig(
-        app_name='second',
-        bundle='com.example',
-        version='0.0.2',
-        description='The second simple app',
-        sources=['src/second'],
+        app_name="second",
+        bundle="com.example",
+        version="0.0.2",
+        description="The second simple app",
+        sources=["src/second"],
     )
 
 
 @pytest.fixture
 def third_app(tmp_path):
     # Make sure the source code exists
-    (tmp_path / 'src' / 'third').mkdir(parents=True, exist_ok=True)
-    with (tmp_path / 'src' / 'third' / '__init__.py').open('w') as f:
+    (tmp_path / "src" / "third").mkdir(parents=True, exist_ok=True)
+    with (tmp_path / "src" / "third" / "__init__.py").open("w") as f:
         f.write('print("Hello world")')
 
     # Create the dist-info folder
-    (tmp_path / 'src' / 'third.dist-info').mkdir(exist_ok=True)
+    (tmp_path / "src" / "third.dist-info").mkdir(exist_ok=True)
 
     return AppConfig(
-        app_name='third',
-        bundle='com.example',
-        version='0.0.2',
-        description='The third simple app',
-        sources=['src/third', 'src2/b'],
+        app_name="third",
+        bundle="com.example",
+        version="0.0.2",
+        description="The third simple app",
+        sources=["src/third", "src2/b"],
     )
--- tests/commands/dev/test_call.py
+++ tests/commands/dev/test_call.py
@@ -10,9 +10,10 @@ class DummyDevCommand(DevCommand):
     A dummy Dev command that doesn't actually do anything.
     It only serves to track which actions would be performend.
     """
-    platform = 'tester'
-    output_format = 'dummy'
-    description = 'Dummy dev command'
+
+    platform = "tester"
+    output_format = "dummy"
+    description = "Dummy dev command"
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, apps=[], **kwargs)
@@ -21,17 +22,14 @@ class DummyDevCommand(DevCommand):
         self.env = dict(a=1, b=2, c=3)
 
     def install_dev_dependencies(self, app, **kwargs):
-        self.actions.append(('dev_dependencies', app.app_name, kwargs))
+        self.actions.append(("dev_dependencies", app.app_name, kwargs))
 
     def get_environment(self, app):
         return self.env
 
     def run_dev_app(self, app, env, **kwargs):
-        self.actions.append(('run_dev', app.app_name, kwargs, env))
-        return full_kwargs({
-            'run_dev_state': app.app_name,
-            'env': env
-        }, kwargs)
+        self.actions.append(("run_dev", app.app_name, kwargs, env))
+        return full_kwargs({"run_dev_state": app.app_name, "env": env}, kwargs)
 
 
 @pytest.fixture
@@ -46,8 +44,7 @@ def test_no_git(dummy_dev_command, first_app):
 
     # The command will fail tool verification.
     with pytest.raises(
-        BriefcaseCommandError,
-        match=r"Briefcase requires git, but it is not installed"
+        BriefcaseCommandError, match=r"Briefcase requires git, but it is not installed"
     ):
         dummy_dev_command()
 
@@ -59,7 +56,7 @@ def test_no_args_one_app(dummy_dev_command, first_app):
     "If there is one app, dev starts that app by default"
     # Add a single app
     dummy_dev_command.apps = {
-        'first': first_app,
+        "first": first_app,
     }
 
     # Configure no command line options
@@ -71,7 +68,7 @@ def test_no_args_one_app(dummy_dev_command, first_app):
     # The right sequence of things will be done
     assert dummy_dev_command.actions == [
         # Run the first app devly
-        ('run_dev', 'first', {'verbosity': 1}, dummy_dev_command.env),
+        ("run_dev", "first", {"verbosity": 1}, dummy_dev_command.env),
     ]
 
 
@@ -79,8 +76,8 @@ def test_no_args_two_apps(dummy_dev_command, first_app, second_app):
     "If there are one app, dev starts that app by default"
     # Add two apps
     dummy_dev_command.apps = {
-        'first': first_app,
-        'second': second_app,
+        "first": first_app,
+        "second": second_app,
     }
 
     # Configure no command line options
@@ -98,11 +95,11 @@ def test_with_arg_one_app(dummy_dev_command, first_app):
     "If there is one app, and a -a argument, dev starts that app"
     # Add a single app
     dummy_dev_command.apps = {
-        'first': first_app,
+        "first": first_app,
     }
 
     # Configure a -a command line option
-    options = dummy_dev_command.parse_options(['-a', 'first'])
+    options = dummy_dev_command.parse_options(["-a", "first"])
 
     # Run the run command
     dummy_dev_command(**options)
@@ -110,7 +107,7 @@ def test_with_arg_one_app(dummy_dev_command, first_app):
     # The right sequence of things will be done
     assert dummy_dev_command.actions == [
         # Run the first app devly
-        ('run_dev', 'first', {'verbosity': 1}, dummy_dev_command.env),
+        ("run_dev", "first", {"verbosity": 1}, dummy_dev_command.env),
     ]
 
 
@@ -118,12 +115,12 @@ def test_with_arg_two_apps(dummy_dev_command, first_app, second_app):
     "If there are multiple apps, the --app argument starts app nominated"
     # Add two apps
     dummy_dev_command.apps = {
-        'first': first_app,
-        'second': second_app,
+        "first": first_app,
+        "second": second_app,
     }
 
     # Configure a --app command line option
-    options = dummy_dev_command.parse_options(['--app', 'second'])
+    options = dummy_dev_command.parse_options(["--app", "second"])
 
     # Run the run command
     dummy_dev_command(**options)
@@ -131,7 +128,7 @@ def test_with_arg_two_apps(dummy_dev_command, first_app, second_app):
     # The right sequence of things will be done
     assert dummy_dev_command.actions == [
         # Run the second app devly
-        ('run_dev', 'second', {'verbosity': 1}, dummy_dev_command.env),
+        ("run_dev", "second", {"verbosity": 1}, dummy_dev_command.env),
     ]
 
 
@@ -139,12 +136,12 @@ def test_bad_app_reference(dummy_dev_command, first_app, second_app):
     "If the command line argument refers to an app that doesn't exist, raise an error"
     # Add two apps
     dummy_dev_command.apps = {
-        'first': first_app,
-        'second': second_app,
+        "first": first_app,
+        "second": second_app,
     }
 
     # Configure a --app command line option
-    options = dummy_dev_command.parse_options(['--app', 'does-not-exist'])
+    options = dummy_dev_command.parse_options(["--app", "does-not-exist"])
 
     # Invoking the run command raises an error
     with pytest.raises(BriefcaseCommandError):
@@ -158,11 +155,11 @@ def test_update_dependencies(dummy_dev_command, first_app):
     "The dev command can request that the app is updated first"
     # Add a single app
     dummy_dev_command.apps = {
-        'first': first_app,
+        "first": first_app,
     }
 
     # Configure no command line options
-    options = dummy_dev_command.parse_options(['-d'])
+    options = dummy_dev_command.parse_options(["-d"])
 
     # Run the run command
     dummy_dev_command(**options)
@@ -170,10 +167,9 @@ def test_update_dependencies(dummy_dev_command, first_app):
     # The right sequence of things will be done
     assert dummy_dev_command.actions == [
         # An update was requested
-        ('dev_dependencies', 'first', {'verbosity': 1}),
-
+        ("dev_dependencies", "first", {"verbosity": 1}),
         # Then, it will be started
-        ('run_dev', 'first', {'verbosity': 1}, dummy_dev_command.env),
+        ("run_dev", "first", {"verbosity": 1}, dummy_dev_command.env),
     ]
 
 
@@ -181,7 +177,7 @@ def test_run_uninstalled(dummy_dev_command, first_app_uninstalled):
     "The dev command will install first if the app hasn't been installed"
     # Add a single app
     dummy_dev_command.apps = {
-        'first': first_app_uninstalled,
+        "first": first_app_uninstalled,
     }
 
     # Configure no command line options
@@ -193,10 +189,9 @@ def test_run_uninstalled(dummy_dev_command, first_app_uninstalled):
     # The right sequence of things will be done
     assert dummy_dev_command.actions == [
         # The app will be installed
-        ('dev_dependencies', 'first', {'verbosity': 1}),
-
+        ("dev_dependencies", "first", {"verbosity": 1}),
         # Then, it will be started
-        ('run_dev', 'first', {'verbosity': 1}, dummy_dev_command.env),
+        ("run_dev", "first", {"verbosity": 1}, dummy_dev_command.env),
     ]
 
 
@@ -204,11 +199,11 @@ def test_update_uninstalled(dummy_dev_command, first_app_uninstalled):
     "A request to update dependencies is redundant if the app hasn't been installed"
     # Add a single app
     dummy_dev_command.apps = {
-        'first': first_app_uninstalled,
+        "first": first_app_uninstalled,
     }
 
     # Configure no command line options
-    options = dummy_dev_command.parse_options(['-d'])
+    options = dummy_dev_command.parse_options(["-d"])
 
     # Run the run command
     dummy_dev_command(**options)
@@ -216,8 +211,7 @@ def test_update_uninstalled(dummy_dev_command, first_app_uninstalled):
     # The right sequence of things will be done
     assert dummy_dev_command.actions == [
         # An update was requested
-        ('dev_dependencies', 'first', {'verbosity': 1}),
-
+        ("dev_dependencies", "first", {"verbosity": 1}),
         # Then, it will be started
-        ('run_dev', 'first', {'verbosity': 1}, dummy_dev_command.env),
+        ("run_dev", "first", {"verbosity": 1}, dummy_dev_command.env),
     ]
