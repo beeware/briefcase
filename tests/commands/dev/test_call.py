7d61c70e84542dffe48e997d0dee8fb6c12fa142
Sagi Shadur <saroad2@gmail.com>
2020-03-11 15:39:49 +0200

    renaming fixtures of dev command tests according to code review.

--- tests/commands/dev/test_call.py
+++ tests/commands/dev/test_call.py
@@ -33,185 +33,185 @@ class DummyDevCommand(DevCommand):
 
 
 @pytest.fixture
-def dummy_dev_command(tmp_path):
+def dev_command(tmp_path):
     return DummyDevCommand(base_path=tmp_path)
 
 
-def test_no_git(dummy_dev_command, first_app):
+def test_no_git(dev_command, first_app):
     "If Git is not installed, an error is raised"
     # Mock a non-existent git
-    dummy_dev_command.git = None
+    dev_command.git = None
 
     # The command will fail tool verification.
     with pytest.raises(
         BriefcaseCommandError, match=r"Briefcase requires git, but it is not installed"
     ):
-        dummy_dev_command()
+        dev_command()
 
     # No apps will be launched
-    assert dummy_dev_command.actions == []
+    assert dev_command.actions == []
 
 
-def test_no_args_one_app(dummy_dev_command, first_app):
+def test_no_args_one_app(dev_command, first_app):
     "If there is one app, dev starts that app by default"
     # Add a single app
-    dummy_dev_command.apps = {
+    dev_command.apps = {
         "first": first_app,
     }
 
     # Configure no command line options
-    options = dummy_dev_command.parse_options([])
+    options = dev_command.parse_options([])
 
     # Run the run command
-    dummy_dev_command(**options)
+    dev_command(**options)
 
     # The right sequence of things will be done
-    assert dummy_dev_command.actions == [
+    assert dev_command.actions == [
         # Run the first app devly
-        ("run_dev", "first", {"verbosity": 1}, dummy_dev_command.env),
+        ("run_dev", "first", {"verbosity": 1}, dev_command.env),
     ]
 
 
-def test_no_args_two_apps(dummy_dev_command, first_app, second_app):
+def test_no_args_two_apps(dev_command, first_app, second_app):
     "If there are one app, dev starts that app by default"
     # Add two apps
-    dummy_dev_command.apps = {
+    dev_command.apps = {
         "first": first_app,
         "second": second_app,
     }
 
     # Configure no command line options
-    options = dummy_dev_command.parse_options([])
+    options = dev_command.parse_options([])
 
     # Invoking the run command raises an error
     with pytest.raises(BriefcaseCommandError):
-        dummy_dev_command(**options)
+        dev_command(**options)
 
     # No apps will be launched
-    assert dummy_dev_command.actions == []
+    assert dev_command.actions == []
 
 
-def test_with_arg_one_app(dummy_dev_command, first_app):
+def test_with_arg_one_app(dev_command, first_app):
     "If there is one app, and a -a argument, dev starts that app"
     # Add a single app
-    dummy_dev_command.apps = {
+    dev_command.apps = {
         "first": first_app,
     }
 
     # Configure a -a command line option
-    options = dummy_dev_command.parse_options(["-a", "first"])
+    options = dev_command.parse_options(["-a", "first"])
 
     # Run the run command
-    dummy_dev_command(**options)
+    dev_command(**options)
 
     # The right sequence of things will be done
-    assert dummy_dev_command.actions == [
+    assert dev_command.actions == [
         # Run the first app devly
-        ("run_dev", "first", {"verbosity": 1}, dummy_dev_command.env),
+        ("run_dev", "first", {"verbosity": 1}, dev_command.env),
     ]
 
 
-def test_with_arg_two_apps(dummy_dev_command, first_app, second_app):
+def test_with_arg_two_apps(dev_command, first_app, second_app):
     "If there are multiple apps, the --app argument starts app nominated"
     # Add two apps
-    dummy_dev_command.apps = {
+    dev_command.apps = {
         "first": first_app,
         "second": second_app,
     }
 
     # Configure a --app command line option
-    options = dummy_dev_command.parse_options(["--app", "second"])
+    options = dev_command.parse_options(["--app", "second"])
 
     # Run the run command
-    dummy_dev_command(**options)
+    dev_command(**options)
 
     # The right sequence of things will be done
-    assert dummy_dev_command.actions == [
+    assert dev_command.actions == [
         # Run the second app devly
-        ("run_dev", "second", {"verbosity": 1}, dummy_dev_command.env),
+        ("run_dev", "second", {"verbosity": 1}, dev_command.env),
     ]
 
 
-def test_bad_app_reference(dummy_dev_command, first_app, second_app):
+def test_bad_app_reference(dev_command, first_app, second_app):
     "If the command line argument refers to an app that doesn't exist, raise an error"
     # Add two apps
-    dummy_dev_command.apps = {
+    dev_command.apps = {
         "first": first_app,
         "second": second_app,
     }
 
     # Configure a --app command line option
-    options = dummy_dev_command.parse_options(["--app", "does-not-exist"])
+    options = dev_command.parse_options(["--app", "does-not-exist"])
 
     # Invoking the run command raises an error
     with pytest.raises(BriefcaseCommandError):
-        dummy_dev_command(**options)
+        dev_command(**options)
 
     # No apps will be launched
-    assert dummy_dev_command.actions == []
+    assert dev_command.actions == []
 
 
-def test_update_dependencies(dummy_dev_command, first_app):
+def test_update_dependencies(dev_command, first_app):
     "The dev command can request that the app is updated first"
     # Add a single app
-    dummy_dev_command.apps = {
+    dev_command.apps = {
         "first": first_app,
     }
 
     # Configure no command line options
-    options = dummy_dev_command.parse_options(["-d"])
+    options = dev_command.parse_options(["-d"])
 
     # Run the run command
-    dummy_dev_command(**options)
+    dev_command(**options)
 
     # The right sequence of things will be done
-    assert dummy_dev_command.actions == [
+    assert dev_command.actions == [
         # An update was requested
         ("dev_dependencies", "first", {"verbosity": 1}),
         # Then, it will be started
-        ("run_dev", "first", {"verbosity": 1}, dummy_dev_command.env),
+        ("run_dev", "first", {"verbosity": 1}, dev_command.env),
     ]
 
 
-def test_run_uninstalled(dummy_dev_command, first_app_uninstalled):
+def test_run_uninstalled(dev_command, first_app_uninstalled):
     "The dev command will install first if the app hasn't been installed"
     # Add a single app
-    dummy_dev_command.apps = {
+    dev_command.apps = {
         "first": first_app_uninstalled,
     }
 
     # Configure no command line options
-    options = dummy_dev_command.parse_options([])
+    options = dev_command.parse_options([])
 
     # Run the run command
-    dummy_dev_command(**options)
+    dev_command(**options)
 
     # The right sequence of things will be done
-    assert dummy_dev_command.actions == [
+    assert dev_command.actions == [
         # The app will be installed
         ("dev_dependencies", "first", {"verbosity": 1}),
         # Then, it will be started
-        ("run_dev", "first", {"verbosity": 1}, dummy_dev_command.env),
+        ("run_dev", "first", {"verbosity": 1}, dev_command.env),
     ]
 
 
-def test_update_uninstalled(dummy_dev_command, first_app_uninstalled):
+def test_update_uninstalled(dev_command, first_app_uninstalled):
     "A request to update dependencies is redundant if the app hasn't been installed"
     # Add a single app
-    dummy_dev_command.apps = {
+    dev_command.apps = {
         "first": first_app_uninstalled,
     }
 
     # Configure no command line options
-    options = dummy_dev_command.parse_options(["-d"])
+    options = dev_command.parse_options(["-d"])
 
     # Run the run command
-    dummy_dev_command(**options)
+    dev_command(**options)
 
     # The right sequence of things will be done
-    assert dummy_dev_command.actions == [
+    assert dev_command.actions == [
         # An update was requested
         ("dev_dependencies", "first", {"verbosity": 1}),
         # Then, it will be started
-        ("run_dev", "first", {"verbosity": 1}, dummy_dev_command.env),
+        ("run_dev", "first", {"verbosity": 1}, dev_command.env),
     ]
--- tests/commands/dev/test_run_dev_app.py
+++ tests/commands/dev/test_run_dev_app.py
@@ -5,21 +5,21 @@ import pytest
 from briefcase.exceptions import BriefcaseCommandError
 
 
-def test_subprocess_running_successfully(dev_command, first_app):
+def test_subprocess_running_successfully(dev_command_with_dummy_subprocess, first_app):
     env = dict(a=1, b=2, c=3)
-    dev_command.run_dev_app(first_app, env)
-    dev_command.subprocess.run.assert_called_once_with(
+    dev_command_with_dummy_subprocess.run_dev_app(first_app, env)
+    dev_command_with_dummy_subprocess.subprocess.run.assert_called_once_with(
         [sys.executable, "-m", first_app.app_name], env=env, check=True
     )
 
 
-def test_subprocess_throws_error(dev_command, first_app):
+def test_subprocess_throws_error(dev_command_with_dummy_subprocess, first_app):
     env = dict(a=1, b=2, c=3)
-    dev_command.subprocess.run.side_effect = CalledProcessError(returncode=2, cmd="cmd")
+    dev_command_with_dummy_subprocess.subprocess.run.side_effect = CalledProcessError(returncode=2, cmd="cmd")
     with pytest.raises(
         BriefcaseCommandError, match="Unable to start application 'first'"
     ):
-        dev_command.run_dev_app(first_app, env)
-    dev_command.subprocess.run.assert_called_once_with(
+        dev_command_with_dummy_subprocess.run_dev_app(first_app, env)
+    dev_command_with_dummy_subprocess.subprocess.run.assert_called_once_with(
         [sys.executable, "-m", first_app.app_name], env=env, check=True
     )
