88451f92632adaea9a5d3461342dcc2034201a4f
Xing Yang <x.yang89@yahoo.com>
2021-10-29 09:56:17 -0700

    Added tests that includes hyphens.

--- tests/commands/new/test_make_app_name.py
+++ tests/commands/new/test_make_app_name.py
@@ -11,7 +11,9 @@ import pytest
         ('98 Hello World', 'helloworld'),
         ('Hello World_', 'helloworld'),
         ('Hello world.', 'helloworld'),
-        ('_HelloWorld_', 'helloworld')
+        ('_HelloWorld_', 'helloworld'),
+        ('Hello_World_', 'hello_world'),
+        ('Hello World-', 'helloworld'),
     ]
 )
 def test_make_app_name(new_command, formal_name, candidate):
