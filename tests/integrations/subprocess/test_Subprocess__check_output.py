0f021a2a1877c6dd0b7f2e1bfbd94d7908715753
Russell Keith-Magee <russell@keith-magee.com>
2022-04-28 14:47:16 +0800

    Cosmetic cleanups to subprocess tests, plus extra tests of merged environments on deep debug.

--- tests/integrations/subprocess/test_Subprocess__check_output.py
+++ tests/integrations/subprocess/test_Subprocess__check_output.py
@@ -6,7 +6,7 @@ import pytest
 from briefcase.console import Log
 
 
-def test_simple_call(mock_sub, capsys):
+def test_call(mock_sub, capsys):
     "A simple call will be invoked"
 
     mock_sub.check_output(['hello', 'world'])
@@ -15,7 +15,7 @@ def test_simple_call(mock_sub, capsys):
     assert capsys.readouterr().out == ""
 
 
-def test_simple_call_with_arg(mock_sub, capsys):
+def test_call_with_arg(mock_sub, capsys):
     "Any extra keyword arguments are passed through as-is"
 
     mock_sub.check_output(['hello', 'world'], universal_newlines=True)
@@ -27,7 +27,7 @@ def test_simple_call_with_arg(mock_sub, capsys):
     assert capsys.readouterr().out == ""
 
 
-def test_simple_call_with_path_arg(mock_sub, capsys, tmp_path):
+def test_call_with_path_arg(mock_sub, capsys, tmp_path):
     "Path-based arguments are converted to strings andpassed in as-is"
 
     mock_sub.check_output(['hello', tmp_path / 'location'], cwd=tmp_path / 'cwd')
@@ -39,8 +39,8 @@ def test_simple_call_with_path_arg(mock_sub, capsys, tmp_path):
     assert capsys.readouterr().out == ""
 
 
-def test_simple_debug_call(mock_sub, capsys):
-    "If verbosity is turned out, there is output"
+def test_debug_call(mock_sub, capsys):
+    "If verbosity is turned up, there is output"
     mock_sub.command.logger = Log(verbosity=2)
 
     mock_sub.check_output(['hello', 'world'])
@@ -50,8 +50,8 @@ def test_simple_debug_call(mock_sub, capsys):
     assert capsys.readouterr().out == "\n>>> Running Command:\n>>>     hello world\n"
 
 
-def test_simple_debug_call_with_env(mock_sub, capsys):
-    "If verbosity is turned out, and injected env vars are included output"
+def test_debug_call_with_env(mock_sub, capsys):
+    "If verbosity is turned up, injected env vars are included in output"
     mock_sub.command.logger = Log(verbosity=2)
 
     env = {"NewVar": "NewVarValue"}
@@ -59,6 +59,7 @@ def test_simple_debug_call_with_env(mock_sub, capsys):
 
     merged_env = mock_sub.command.os.environ.copy()
     merged_env.update(env)
+
     mock_sub._subprocess.check_output.assert_called_with(['hello', 'world'], env=merged_env)
 
     expected_output = (
@@ -72,8 +73,8 @@ def test_simple_debug_call_with_env(mock_sub, capsys):
     assert capsys.readouterr().out == expected_output
 
 
-def test_simple_deep_debug_call(mock_sub, capsys):
-    "If verbosity is turned out, there is output"
+def test_deep_debug_call(mock_sub, capsys):
+    "If verbosity is at the max, the full environment and return is output"
     mock_sub.command.logger = Log(verbosity=3)
 
     mock_sub.check_output(["hello", "world"])
@@ -91,6 +92,39 @@ def test_simple_deep_debug_call(mock_sub, capsys):
         ">>> \n"
         ">>> Line 4\n"
         ">>>     PWD=/home/user/\n"
+        ">>> Command Output:\n"
+        ">>>     some output line 1\n"
+        ">>>     more output line 2\n"
+        ">>> Return code: 0\n"
+    )
+
+    assert capsys.readouterr().out == expected_output
+
+
+def test_deep_debug_call_with_env(mock_sub, capsys):
+    "If verbosity is at the max, the full environment and return is output, and the environment is merged"
+    mock_sub.command.logger = Log(verbosity=3)
+
+    env = {"NewVar": "NewVarValue"}
+    mock_sub.check_output(['hello', 'world'], env=env)
+
+    merged_env = mock_sub.command.os.environ.copy()
+    merged_env.update(env)
+
+    mock_sub._subprocess.check_output.assert_called_with(["hello", "world"], env=merged_env)
+
+    expected_output = (
+        "\n"
+        ">>> Running Command:\n"
+        ">>>     hello world\n"
+        ">>> Full Environment:\n"
+        ">>>     VAR1=Value 1\n"
+        ">>>     PS1=\n"
+        ">>> Line 2\n"
+        ">>> \n"
+        ">>> Line 4\n"
+        ">>>     PWD=/home/user/\n"
+        ">>>     NewVar=NewVarValue\n"
         ">>> Command Output:\n"
         ">>>     some output line 1\n"
         ">>>     more output line 2\n"
