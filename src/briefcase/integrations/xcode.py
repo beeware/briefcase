34cc521c83e432d68e93a68086a0ff452eba3113
Dan Yeaw <dan@yeaw.me>
2022-05-24 19:46:12 -0400

    Return dict instead of set of pairs

--- src/briefcase/integrations/xcode.py
+++ src/briefcase/integrations/xcode.py
@@ -471,11 +471,9 @@ def get_identities(command, policy):
         )
 
         return {
-            (
                 IDENTITY_RE.match(line).groups()
                 for line in output.split('\n')
                 if IDENTITY_RE.match(line)
-            )
         }
 
     except subprocess.CalledProcessError as e:
