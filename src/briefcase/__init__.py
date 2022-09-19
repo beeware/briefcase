44fb5b7efffa92bfedcbf33d07b3314516d25461
Russell Keith-Magee <russell@keith-magee.com>
2022-09-19 11:24:28 +0800

    Use setuptools_scm for version metadata.

--- src/briefcase/__init__.py
+++ src/briefcase/__init__.py
@@ -1,13 +1,8 @@
+from importlib.metadata import version
+
 __all__ = [
     "__version__",
 ]
 
-# Examples of valid version strings
-# __version__ = '1.2.3.dev1'  Development release 1
-# __version__ = '1.2.3a1'     Alpha Release 1
-# __version__ = '1.2.3b1'     Beta Release 1
-# __version__ = '1.2.3rc1'    RC Release 1
-# __version__ = '1.2.3'       Final Release
-# __version__ = '1.2.3.post1' Post Release 1
-
-__version__ = "0.3.9"
+# Read version from SCM metadata
+__version__ = version("briefcase")
