97f1a338a67325150f83e0833a62310a7e727fb1
Russell Keith-Magee <russell@keith-magee.com>
2022-09-20 22:02:41 +0800

    Catch the case where setuptools_scm is in the environment, but the code isn't a git checkout.

--- src/briefcase/__init__.py
+++ src/briefcase/__init__.py
@@ -8,8 +8,11 @@ try:
     from setuptools_scm import get_version
 
     __version__ = get_version()
-except ModuleNotFoundError:
-    # Read version from the installer metadata
+except (ModuleNotFoundError, LookupError):
+    # If setuptools_scm isn't in the environment, the call to import will fail.
+    # If it *is* in the environment, but the code isn't a git checkout (e.g.,
+    # it's been pip installed non-editable) the call to get_version() will fail.
+    # If either of these occurs, read version from the installer metadata.
     from importlib.metadata import version
 
     __version__ = version("briefcase")
