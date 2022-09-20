cce6173fed27053a9664bc1573dc7bf5a31df367
Russell Keith-Magee <russell@keith-magee.com>
2022-09-20 17:46:33 +0800

    Revised setuptools_scm usage.

--- src/briefcase/__init__.py
+++ src/briefcase/__init__.py
@@ -1,8 +1,15 @@
-from importlib.metadata import version
-
 __all__ = [
     "__version__",
 ]
 
-# Read version from SCM metadata
-__version__ = version("briefcase")
+try:
+    # Read version from SCM metadata
+    # This will only exist in a development environment
+    from setuptools_scm import get_version
+
+    __version__ = get_version()
+except ModuleNotFoundError:
+    # Read version from the installer metadata
+    from importlib.metadata import version
+
+    __version__ = version("briefcase")
