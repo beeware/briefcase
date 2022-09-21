c1bcfeb3f0a8d6dbdc1ca2e914f0acf0aa43f799
Russell Keith-Magee <russell@keith-magee.com>
2022-09-21 14:51:11 +0800

    Use an explicit SCM configuration path.

--- src/briefcase/__init__.py
+++ src/briefcase/__init__.py
@@ -7,7 +7,7 @@ try:
     # This will only exist in a development environment
     from setuptools_scm import get_version
 
-    __version__ = get_version()
+    __version__ = get_version("../..", relative_to=__file__)
 except (ModuleNotFoundError, LookupError):
     # If setuptools_scm isn't in the environment, the call to import will fail.
     # If it *is* in the environment, but the code isn't a git checkout (e.g.,
