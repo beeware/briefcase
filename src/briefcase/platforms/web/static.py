ececf1a7a1163b436eb6217240772f6dd5ff4474
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2023-04-24 16:36:33 -0600

    keep wasm32 wheels

--- src/briefcase/platforms/web/static.py
+++ src/briefcase/platforms/web/static.py
@@ -183,10 +183,11 @@ class StaticWebBuildCommand(StaticWebMixin, BuildCommand):
                 }
                 # Ensure that non-pyodide-compatible wheels are passed as package names
                 # so that micropip can try to install it from the pyodide index.
-                # TODO: get pyodide whl and bundle it instead of relying on micropip
+                # TODO: get pyodide whl so it can be bundled
                 for wheel in packages:
                     name, version, build, tags = parse_wheel_filename(wheel.name)
-                    if not any(tag.platform == "any" for tag in tags):
+                    if not any(tag.platform == "any" or "wasm32" in tag.platform
+                               for tag in tags):
                         packages[wheel] = name
                 config = {
                     "name": app.formal_name,
