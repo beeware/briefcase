40f64e174a84e139537cbb3e346286512a0b311a
Gareth Maddock <gtmaddock@gmail.com>
2024-05-21 08:52:00 -0400

    add test to show info.plist camera permissions not being added

--- tests/platforms/macOS/app/test_create.py
+++ tests/platforms/macOS/app/test_create.py
@@ -73,7 +73,9 @@ def create_command(tmp_path, first_app_templated):
             {},
             {},
             {
-                "info": {},
+                "info": {
+                    "NSCameraUsageDescription": "I need to see you",
+                },
                 "entitlements": {
                     "com.apple.security.cs.allow-unsigned-executable-memory": True,
                     "com.apple.security.cs.disable-library-validation": True,
