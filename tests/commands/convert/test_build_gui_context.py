d96066fbf84446be45ffd1d07f41dd924a13c9bf
installer <installer@gmail.com>
2024-12-27 00:40:08 +0530

    test modification

--- tests/commands/convert/test_build_gui_context.py
+++ tests/commands/convert/test_build_gui_context.py
@@ -1,5 +1,6 @@
-def test_overrides_are_not_used(convert_command):
-    overrides = {"gui_framework": "Toga"}
+def test_overrides_are_used(convert_command):
+    overrides = {"gui_framework": "Toga", "console_app": "true"}
     out = convert_command.build_gui_context({}, overrides)
-    assert out == {"gui_framework": "None"}
-    assert overrides == {"gui_framework": "Toga"}
+    assert out["gui_framework"] == "None"
+    assert overrides["gui_framework"] == "Toga"
+    assert out["console_app"] == "true"
