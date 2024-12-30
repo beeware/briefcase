c4ac24a42f5c5a6e220c7eda579b314ec36bc3fc
logan-keede <68557630+logan-keede@users.noreply.github.com>
2024-12-30 14:41:42 +0530

    test modification

--- tests/commands/convert/test_build_gui_context.py
+++ tests/commands/convert/test_build_gui_context.py
@@ -1,5 +1,5 @@
 def test_overrides_are_used(convert_command):
     overrides = {"gui_framework": "Toga", "console_app": "Console"}
     out = convert_command.build_gui_context({}, overrides)
-    assert out == {"gui_framework": "None", "console_app": "true"}
+    assert out == {"gui_framework": "None", "console_app": True}
     assert overrides == {"gui_framework": "Toga"}
--- tests/commands/convert/test_input_console_app.py
+++ tests/commands/convert/test_input_console_app.py
@@ -4,13 +4,13 @@ from unittest.mock import MagicMock
 def test_overrides_are_used_for_console(convert_command):
     overrides = {"console_app": "Console"}
     out = convert_command.build_gui_context({}, overrides)
-    assert out["console_app"] == "true"
+    assert out["console_app"]
 
 
 def test_overrides_are_used_for_GUI(convert_command):
     overrides = {"console_app": "GUI"}
     out = convert_command.build_gui_context({}, overrides)
-    assert out["console_app"] == "false"
+    assert not out["console_app"]
 
 
 def test_input_called_properly(convert_command, monkeypatch):
