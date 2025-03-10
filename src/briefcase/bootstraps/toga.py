b2b71115f5e7a3bff616436de8a79c726a89d7d7
Russell Keith-Magee <russell@keith-magee.com>
2025-03-10 10:45:49 +0800

    Remove reference to Ubuntu 22.04, which is no longer supported.

--- src/briefcase/bootstraps/toga.py
+++ src/briefcase/bootstraps/toga.py
@@ -92,7 +92,7 @@ system_runtime_requires = [
     # Dependencies that GTK looks for at runtime
     "libcanberra-gtk3-module",
     # Needed to provide WebKit2 at runtime
-    # Note: Debian 11 and Ubuntu 20.04 require gir1.2-webkit2-4.0 instead
+    # Note: Debian 11 requires gir1.2-webkit2-4.0 instead
     # "gir1.2-webkit2-4.1",
 ]
 """
