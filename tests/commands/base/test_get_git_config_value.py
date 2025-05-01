352990bb013cc7a80fc780e02dd05d559b0bb945
ABC <niklasmertsch@web.de>
2025-05-01 20:55:13 +0200

    Add test for get_git_config_value()

--- tests/commands/base/test_get_git_config_value.py
+++ tests/commands/base/test_get_git_config_value.py
@@ -1,4 +1,6 @@
+import itertools
 from unittest import mock
+from unittest.mock import MagicMock
 
 
 def test_all_config_files_are_read(base_command, mock_git):
@@ -15,3 +17,30 @@ def test_all_config_files_are_read(base_command, mock_git):
     ]
     expected_config_files = ["file1", "file2", "file3", ".git/config"]
     mock_git.config.GitConfigParser.assert_called_once_with(expected_config_files)
+
+
+def test_config_values_are_parsed(base_command, tmp_path, monkeypatch):
+    """If the requested value exists in one of the config files, it shall be returned."""
+    import git
+
+    # use 'real' gitpython library (no mock)
+    base_command.tools.git = git
+
+    # mock `git.config.get_config_path` to always provide the same three local files
+    mock_config_paths = ["missing-file-1", "config-1", "missing-file-2"]
+    git.config.get_config_path = MagicMock()
+    git.config.get_config_path.side_effect = itertools.cycle(mock_config_paths)
+
+    # create local two config files
+    monkeypatch.chdir(tmp_path)
+    (tmp_path / "config-1").write_text("[user]\n\tname = Some User\n")
+    (tmp_path / ".git").mkdir()
+    (tmp_path / ".git" / "config").write_text("[user]\n\temail = my@email.com\n")
+
+    # expect values are parsed from all existing config files
+    assert base_command.get_git_config_value("user", "name") == "Some User"
+    assert base_command.get_git_config_value("user", "email") == "my@email.com"
+
+    # expect that missing sections and options are handled
+    assert base_command.get_git_config_value("user", "something") is None
+    assert base_command.get_git_config_value("something", "something") is None
