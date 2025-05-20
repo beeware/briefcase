e076e471c4dd89b34798fc18e5c4ea9aac73d5b6
amit <amiteitan@gmail.com>
2025-05-20 00:24:37 -0400

    update cleanup stub binary test to support new extensions

--- tests/commands/create/test_cleanup_stub_binary.py
+++ tests/commands/create/test_cleanup_stub_binary.py
@@ -1,6 +1,15 @@
+import sys
+
 import pytest
 
 from ...utils import create_file
+
+
+def get_stub_name():
+    stub = "Stub"
+    if sys.platform == "win32":
+        stub += "."
+    return stub
 
 
 @pytest.mark.parametrize(
@@ -14,11 +23,10 @@ from ...utils import create_file
 )
 def test_cleanup_stubs(create_command, unbuilt, built, myapp, tmp_path):
     """If an unbuilt stub already exists, it is cleaned up."""
-
     # Mock a existing stubs
     if unbuilt:
         create_file(
-            tmp_path / "base_path/build/my-app/tester/dummy/Stub.bin",
+            tmp_path / "base_path/build/my-app/tester/dummy" / get_stub_name(),
             "Unbuilt stub",
         )
     if built:
@@ -31,5 +39,9 @@ def test_cleanup_stubs(create_command, unbuilt, built, myapp, tmp_path):
     create_command.cleanup_stub_binary(myapp)
 
     # No matter the starting state, the stubs don't exist after cleanup.
-    assert not (tmp_path / "base_path/build/my-app/tester/dummy/Stub.bin").exists()
-    assert not (tmp_path / "base_path/build/my-app/tester/dummy/my-app.bin").exists()
+    assert not (
+        tmp_path / "base_path/build/my-app/tester/dummy" / get_stub_name()
+    ).exists()
+    assert not (
+        tmp_path / "base_path/build/my-app/tester/dummy" / myapp.formal_name
+    ).exists()
