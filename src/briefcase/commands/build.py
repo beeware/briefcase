21ec96c56e552c18cdfd70f55e2cd19778c8e7d2
Tim Rid <6593626+timrid@users.noreply.github.com>
2025-06-04 20:29:09 +0200

    moved test_mode Flag to AppConfig

--- src/briefcase/commands/build.py
+++ src/briefcase/commands/build.py
@@ -40,7 +40,6 @@ class BuildCommand(BaseCommand):
         update_support: bool,
         update_stub: bool,
         no_update: bool,
-        test_mode: bool,
         **options,
     ) -> dict | None:
         """Internal method to invoke a build on a single app. Ensures the app exists,
@@ -56,10 +55,9 @@ class BuildCommand(BaseCommand):
         :param update_support: Should the application support be updated?
         :param update_stub: Should the stub binary be updated?
         :param no_update: Should automated updates be disabled?
-        :param test_mode: Is the app being build in test mode?
         """
         if not self.bundle_path(app).exists():
-            state = self.create_command(app, test_mode=test_mode, **options)
+            state = self.create_command(app, **options)
         elif (
             update  # An explicit update has been requested
             or update_requirements  # An explicit update of requirements has been requested
@@ -67,7 +65,7 @@ class BuildCommand(BaseCommand):
             or update_support  # An explicit update of app support has been requested
             or update_stub  # An explicit update of the stub binary has been requested
             or (
-                test_mode and not no_update
+                app.test_mode and not no_update
             )  # Test mode, but updates have not been disabled
         ):
             state = self.update_command(
@@ -76,7 +74,6 @@ class BuildCommand(BaseCommand):
                 update_resources=update_resources,
                 update_support=update_support,
                 update_stub=update_stub,
-                test_mode=test_mode,
                 **options,
             )
         else:
@@ -84,9 +81,9 @@ class BuildCommand(BaseCommand):
 
         self.verify_app(app)
 
-        state = self.build_app(app, test_mode=test_mode, **full_options(state, options))
+        state = self.build_app(app, **full_options(state, options))
 
-        qualifier = " (test mode)" if test_mode else ""
+        qualifier = " (test mode)" if app.test_mode else ""
         self.console.info(
             f"Built {self.binary_path(app).relative_to(self.base_path)}{qualifier}",
             prefix=app.app_name,
@@ -132,7 +129,7 @@ class BuildCommand(BaseCommand):
 
         # Confirm host compatibility, that all required tools are available,
         # and that the app configuration is finalized.
-        self.finalize(app)
+        self.finalize(app, test_mode)
 
         if app_name:
             try:
@@ -156,7 +153,6 @@ class BuildCommand(BaseCommand):
                 update_support=update_support,
                 update_stub=update_stub,
                 no_update=no_update,
-                test_mode=test_mode,
                 **full_options(state, options),
             )
 
