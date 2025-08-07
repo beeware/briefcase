889c002fabbdf8315784c737de4be25270c33136
Russell Keith-Magee <russell@keith-magee.com>
2025-08-07 15:11:34 +0800

    Correct support package revision implementation.

--- src/briefcase/platforms/windows/__init__.py
+++ src/briefcase/platforms/windows/__init__.py
@@ -58,7 +58,7 @@ class WindowsCreateCommand(CreateCommand):
         return f"python-{self.python_version_tag}.{support_revision}-embed-amd64.zip"
 
     def support_package_url(self, support_revision):
-        micro = re.match(r"\d+", "0rc1").group(0)
+        micro = re.match(r"\d+", support_revision).group(0)
         return (
             f"https://www.python.org/ftp/python/"
             f"{self.python_version_tag}.{micro}/"
