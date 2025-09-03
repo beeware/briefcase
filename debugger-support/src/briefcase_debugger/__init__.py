550a3d134d52fbfa41226048db9d9e78c5dea58d
Tim Rid <6593626+timrid@users.noreply.github.com>
2025-09-03 23:34:07 +0200

    REMOTE_DEBUGGER_STARTED is unnecessary

--- debugger-support/src/briefcase_debugger/__init__.py
+++ debugger-support/src/briefcase_debugger/__init__.py
@@ -3,52 +3,45 @@ import os
 import sys
 import traceback
 
-from briefcase_debugger.config import DebuggerConfig
-
-REMOTE_DEBUGGER_STARTED = False
-
 
 def start_remote_debugger():
-    global REMOTE_DEBUGGER_STARTED
-    REMOTE_DEBUGGER_STARTED = True
-
-    # check verbose output
-    verbose = os.environ.get("BRIEFCASE_DEBUG", "0") == "1"
-
-    # reading config
-    config_str = os.environ.get("BRIEFCASE_DEBUGGER", None)
-
-    # skip debugger if no config is set
-    if config_str is None:
-        if verbose:
-            print(
-                "No 'BRIEFCASE_DEBUGGER' environment variable found. Debugger not starting."
-            )
-        return  # If BRIEFCASE_DEBUGGER is not set, this packages does nothing...
-
-    if verbose:
-        print(f"'BRIEFCASE_DEBUGGER'={config_str}")
-
-    # Parsing config json
-    config: DebuggerConfig = json.loads(config_str)
-
-    # start debugger
-    print("Starting remote debugger...")
-    if config["debugger"] == "debugpy":
-        from briefcase_debugger.debugpy import start_debugpy
-
-        start_debugpy(config, verbose)
-    elif config["debugger"] == "pdb":
-        from briefcase_debugger.pdb import start_pdb
-
-        start_pdb(config, verbose)
-
-
-# only start remote debugger on the first import
-if not REMOTE_DEBUGGER_STARTED:
     try:
-        start_remote_debugger()
+        # check verbose output
+        verbose = os.environ.get("BRIEFCASE_DEBUG", "0") == "1"
+
+        # reading config
+        config_str = os.environ.get("BRIEFCASE_DEBUGGER", None)
+
+        # skip debugger if no config is set
+        if config_str is None:
+            if verbose:
+                print(
+                    "No 'BRIEFCASE_DEBUGGER' environment variable found. Debugger not starting."
+                )
+            return  # If BRIEFCASE_DEBUGGER is not set, this packages does nothing...
+
+        if verbose:
+            print(f"'BRIEFCASE_DEBUGGER'={config_str}")
+
+        # Parsing config json
+        config = json.loads(config_str)
+
+        # start debugger
+        print("Starting remote debugger...")
+        if config["debugger"] == "debugpy":
+            from briefcase_debugger.debugpy import start_debugpy
+
+            start_debugpy(config, verbose)
+        elif config["debugger"] == "pdb":
+            from briefcase_debugger.pdb import start_pdb
+
+            start_pdb(config, verbose)
+        else:
+            raise ValueError(f"Unknown debugger '{config['debugger']}'")
     except Exception:
         # Show exception and stop the whole application when an error occurs
         print(traceback.format_exc())
         sys.exit(-1)
+
+
+start_remote_debugger()
---
+++ debugger-support/tests/test_base.py
@@ -0,0 +1,47 @@
+import importlib
+import json
+import os
+import sys
+from unittest.mock import MagicMock, call
+
+import briefcase_debugger
+
+
+def test_auto_startup(monkeypatch, capsys):
+    """Test that the debugger startup code is executed on import."""
+    fake_environ = MagicMock()
+    monkeypatch.setattr(os, "environ", fake_environ)
+
+    fake_environ.get.return_value = None
+
+    # reload the module to trigger the auto startup code
+    importlib.reload(importlib.import_module("briefcase_debugger"))
+
+    # check if the autostart was executed
+    assert fake_environ.get.mock_calls == [
+        call("BRIEFCASE_DEBUG", "0"),
+        call("BRIEFCASE_DEBUGGER", None),
+    ]
+
+
+def test_unknown_debugger(monkeypatch, capsys):
+    """Test that an unknown debugger raises an error and stops the application."""
+    os_environ = {}
+    os_environ["BRIEFCASE_DEBUGGER"] = json.dumps(
+        {
+            "debugger": "unknown",
+            "host": "somehost",
+            "port": 9999,
+        }
+    )
+    monkeypatch.setattr(os, "environ", os_environ)
+
+    fake_sys_exit = MagicMock()
+    monkeypatch.setattr(sys, "exit", fake_sys_exit)
+
+    briefcase_debugger.start_remote_debugger()
+
+    fake_sys_exit.assert_called_once_with(-1)
+
+    captured = capsys.readouterr()
+    assert "Unknown debugger" in captured.out
