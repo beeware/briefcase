8c3eb71df93947c6ebd7b43548cd8fb4f9c1540b
Mitchell <mitchell.pontague@hotmail.com>
2025-09-13 22:12:16 +0800

    Modifications to enter/exit tests to remove console mock

--- tests/integrations/virtual_environment/test_VirtualEnvironment__context_manager.py
+++ tests/integrations/virtual_environment/test_VirtualEnvironment__context_manager.py
@@ -15,15 +15,8 @@ def test_context_manager_recreate_true(dummy_tools, dummy_console, tmp_path):
 
     env.venv_context.recreate = MagicMock()
 
-    dummy_console.wait_bar = MagicMock()
-    dummy_console.wait_bar.return_value.__enter__ = MagicMock()
-    dummy_console.wait_bar.return_value.__exit__ = MagicMock(return_value=False)
-
     with env as context:
         assert context == env.venv_context
-        dummy_console.wait_bar.assert_called_once_with(
-            "Recreating virtual environment..."
-        )
         env.venv_context.recreate.assert_called_once()
 
 
@@ -42,18 +35,11 @@ def test_context_manager_recreate_true_exists(dummy_tools, dummy_console, tmp_pa
     env.venv_context.recreate = MagicMock()
     env.venv_context.create = MagicMock()
 
-    dummy_console.wait_bar = MagicMock()
-    dummy_console.wait_bar.return_value.__enter__ = MagicMock()
-    dummy_console.wait_bar.return_value.__exit__ = MagicMock(return_value=False)
-
     with env as context:
         assert context == env.venv_context
         env.venv_context.exists.assert_not_called()
         env.venv_context.recreate.assert_called_once()
         env.venv_context.create.assert_not_called()
-        dummy_console.wait_bar.assert_called_once_with(
-            "Recreating virtual environment..."
-        )
 
 
 def test_context_manager_venv_nonexistent(dummy_tools, dummy_console, tmp_path):
@@ -69,16 +55,9 @@ def test_context_manager_venv_nonexistent(dummy_tools, dummy_console, tmp_path):
     env.venv_context.exists = MagicMock(return_value=False)
     env.venv_context.create = MagicMock()
 
-    dummy_console.wait_bar = MagicMock()
-    dummy_console.wait_bar.return_value.__enter__ = MagicMock()
-    dummy_console.wait_bar.return_value.__exit__ = MagicMock(return_value=False)
-
     with env as context:
         assert context == env.venv_context
         env.venv_context.exists.assert_called_once()
-        dummy_console.wait_bar.assert_called_once_with(
-            f"Creating virtual environment at {venv_path}..."
-        )
         env.venv_context.create.assert_called_once()
 
 
