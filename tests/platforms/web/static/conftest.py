30bf837305427dddcfc2c3f91eed5e756ed1bb82
19766375 <19766375@student.curtin.edu.au>
2025-09-20 18:13:46 +0800

    Remove pyscript.toml from conftest.py

--- tests/platforms/web/static/conftest.py
+++ tests/platforms/web/static/conftest.py
@@ -19,15 +19,6 @@ app_requirements_path="requirements.txt"
     # Create index.html
     create_file(bundle_path / "www/index.html", "<html></html>")
 
-    # Create the initial pyscript.toml
-    create_file(
-        bundle_path / "www/pyscript.toml",
-        """
-existing-key-1 = "value-1"
-existing-key-2 = 2
-""",
-    )
-
     # Create the initial briefcase.css
     create_file(
         bundle_path / "www/static/css/briefcase.css",
@@ -49,12 +40,6 @@ existing-key-2 = 2
 @pytest.fixture
 def first_app_built(first_app_generated, tmp_path):
     bundle_path = tmp_path / "base_path/build/first-app/web/static"
-
-    # Create pyscript.toml
-    create_file(
-        bundle_path / "www/pyscript.toml",
-        'packages = ["dummy-1.2.3-py3-none-all.whl"]',
-    )
 
     # Create an app wheel
     create_wheel(bundle_path / "www/static/wheels")
