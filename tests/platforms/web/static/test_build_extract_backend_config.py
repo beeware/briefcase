699b463d09eff289ab99f41af808e8006f5aa839
19766375 <19766375@student.curtin.edu.au>
2025-10-04 10:18:59 +0800

    test_extract_backend_config_no_pyscript_toml and  test_extract_backend_config_backend_warning

--- tests/platforms/web/static/test_build_extract_backend_config.py
+++ tests/platforms/web/static/test_build_extract_backend_config.py
@@ -139,53 +139,18 @@ def test_extract_backend_config_no_backend(build_command, tmp_path):
         build_command.extract_backend_config([wheel_path])
 
 
-def test_build_app_config_backend_warning(build_command, first_app_generated, tmp_path, capsys):
+def test_extract_backend_config_backend_warning(build_command, tmp_path, capsys):
     """Briefcase raises a warning if "backend" value is not pyscript."""
 
-    bundle_path = tmp_path / "base_path/build/first-app/web/static"
-
-    # Mock some wheels with a single config.toml containing no backend value.
-    def mock_run(*args, **kwargs):
-        if args[0][5] == "wheel":
-            create_wheel(
-                bundle_path / "www/static/wheels",
-                "first_app",
-                extra_content=[
-                    ("dependency/static/style.css", "span { margin: 10px; }\n"),
-                    (
-                        "dependency/deploy/config.toml",
-                        """
+    files = {
+        "dependancy/deploy/config.toml": """
 backend = "something-else"
 """,
-                    ),
-                ],
-            )
-        elif args[0][5] == "pip":
-            create_wheel(
-                bundle_path / "www/static/wheels",
-                "dependency",
-                extra_content=[
-                    ("dependency/static/style.css", "div { margin: 10px; }\n"),
-                ],
-            )
-            create_wheel(
-                bundle_path / "www/static/wheels",
-                "other",
-                extra_content=[
-                    ("other/static/style.css", "div { padding: 10px; }\n"),
-                ],
-            )
-        else:
-            raise ValueError("Unknown command")
+}
 
-    build_command.tools.subprocess.run.side_effect = mock_run
+    wheel_path = _mock_wheel(tmp_path=tmp_path, wheel_name="dependancy", files=files)
 
-    # Mock the side effect of invoking shutil
-    build_command.tools.shutil.rmtree.side_effect = lambda *args: shutil.rmtree(
-        bundle_path / "www/static/wheels"
-    )
-
-    build_command.build_app(first_app_generated)
+    pyscript_config, pyscript_version = build_command.extract_backend_config([wheel_path])
 
     # Capture output and assert warning present.
     captured = capsys.readouterr()
@@ -193,78 +158,28 @@ backend = "something-else"
         "Only 'pyscript' backend is currently supported for web static builds." in captured.out
     )
 
-    # Check pyscript.toml was created and has correct packages.
-    with (bundle_path / "www/pyscript.toml").open("rb") as f:
-        assert tomllib.load(f) == {
-            "packages": [
-                "/static/wheels/dependency-1.2.3-py3-none-any.whl",
-                "/static/wheels/first_app-1.2.3-py3-none-any.whl",
-                "/static/wheels/other-1.2.3-py3-none-any.whl",
-            ],
-        }
+    # Check pyscript_config is empty and pyscript_version is the default.
+    assert pyscript_config == {}
+    assert pyscript_version == "2024.11.1"
 
 
-def test_build_app_no_wheel_pyscript_toml(build_command, first_app_generated, tmp_path):
-    """An app with no pyscript.toml supplied by a wheel gets a basic config."""
+def test_extract_backend_config_no_pyscript_toml(build_command, tmp_path):
+    """If no pyscript.toml is supplied by a wheel, function returns a basic config."""
 
-    bundle_path = tmp_path / "base_path/build/first-app/web/static"
-
-    # Mock some wheels without a pyscript.toml
-    def mock_run(*args, **kwargs):
-        if args[0][5] == "wheel":
-            create_wheel(
-                bundle_path / "www/static/wheels",
-                "first_app",
-                extra_content=[
-                    ("dependency/static/style.css", "span { margin: 10px; }\n"),
-                    (
-                        "dependency/deploy/config.toml",
-                        """
+    # Create wheel with no pyscript.toml
+    files = {
+        "dependancy/deploy/config.toml": """
 backend = "pyscript"
-
-[pyscript]
-version = "2024.11.1"
 """,
-                    ),
-                ],
-            )
-        elif args[0][5] == "pip":
-            create_wheel(
-                bundle_path / "www/static/wheels",
-                "dependency",
-                extra_content=[
-                    ("dependency/static/style.css", "div { margin: 10px; }\n"),
-                ],
-            )
-            create_wheel(
-                bundle_path / "www/static/wheels",
-                "other",
-                extra_content=[
-                    ("other/static/style.css", "div { padding: 10px; }\n"),
-                ],
-            )
-        else:
-            raise ValueError("Unknown command")
+}
 
-    build_command.tools.subprocess.run.side_effect = mock_run
+    wheel_path = _mock_wheel(tmp_path=tmp_path, wheel_name="dependancy", files=files)
 
-    # Mock the side effect of invoking shutil
-    build_command.tools.shutil.rmtree.side_effect = lambda *args: shutil.rmtree(
-        bundle_path / "www/static/wheels"
-    )
+    pyscript_config, pyscript_version = build_command.extract_backend_config([wheel_path])
 
-    # Build the web app.
-    build_command.build_app(first_app_generated)
-
-    # Pyscript.toml has been written with only the packages content
-    with (bundle_path / "www/pyscript.toml").open("rb") as f:
-        assert tomllib.load(f) == {
-            "packages": [
-                "/static/wheels/dependency-1.2.3-py3-none-any.whl",
-                "/static/wheels/first_app-1.2.3-py3-none-any.whl",
-                "/static/wheels/other-1.2.3-py3-none-any.whl",
-            ],
-        }
+    # Check pyscript_config is empty and pyscript_version is the default.
+    assert pyscript_config == {}
+    assert pyscript_version == "2024.11.1"
 
 
 def test_build_app_invalid_wheel_pyscript_toml(
