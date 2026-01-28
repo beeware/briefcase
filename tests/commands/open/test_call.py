4dfb04a3c392be3489b8e28d00f7f0f99c210442
kshivani06 <ksshivaniek@gmail.com>
2026-01-28 07:06:46 +0530

    Add -a/--app support to briefcase open command (#2651)
    Co-authored-by: Russell Keith-Magee <russell@keith-magee.com>

--- tests/commands/open/test_call.py
+++ tests/commands/open/test_call.py
@@ -1,3 +1,8 @@
+import pytest
+
+from briefcase.exceptions import BriefcaseCommandError
+
+
 def test_open(open_command, first_app, second_app):
     """The open command can be called."""
     # Configure no command line options
@@ -75,4 +80,67 @@ def test_create_before_open(open_command, tmp_path):
         # App tools are verified
         ("verify-app-tools", "first"),
         ("open", "first"),
+    ]
+
+
+def test_open_app_name(open_command, first_app, second_app):
+    """The open command can be called with a specific app name."""
+    # Configure the -a / --app command line option
+    options, _ = open_command.parse_options(["-a", "first"])
+
+    open_command(**options)
+
+    # The right sequence of things will be done for ONLY the first app
+    assert open_command.actions == [
+        # Host OS is verified
+        ("verify-host",),
+        # Tools are verified
+        ("verify-tools",),
+        # App config has been finalized for all apps
+        ("finalize-app-config", "first"),
+        ("finalize-app-config", "second"),
+        # App template is verified
+        ("verify-app-template", "first"),
+        # App tools are verified
+        ("verify-app-tools", "first"),
+        # open ONLY the first app
+        ("open", "first"),
+    ]
+
+
+def test_open_app_invalid(open_command, first_app, second_app):
+    """If an invalid app name is passed to --app, raise an error."""
+    # Add two apps to the command
+    open_command.apps = {
+        "first": first_app,
+        "second": second_app,
+    }
+
+    # Configure the --app option with an invalid app
+    options, _ = open_command.parse_options(["--app", "invalid"])
+
+    # Running the command should raise an error
+    with pytest.raises(
+        BriefcaseCommandError,
+        match=r"App 'invalid' does not exist in this project.",
+    ):
+        open_command(**options)
+
+
+def test_open_app_none_defined(open_command):
+    """If no apps are defined, do nothing."""
+    # No apps defined in the project
+    open_command.apps = {}
+
+    # Configure no command line options
+    options, _ = open_command.parse_options([])
+
+    # Run the open command
+    result = open_command(**options)
+
+    # The command should return None and only verify host/tools
+    assert result is None
+    assert open_command.actions == [
+        ("verify-host",),
+        ("verify-tools",),
     ]
