84143fd46b8294315ebf55ef9f1cec87ce9b7367
Russell Keith-Magee <russell@keith-magee.com>
2026-05-14 08:18:29 -0700

    Tweak error message and release note.

--- src/briefcase/config.py
+++ src/briefcase/config.py
@@ -648,12 +648,13 @@ class DraftAppConfig(AppConfig):
 
         if not is_valid_bundle_identifier(self.bundle_identifier):
             raise BriefcaseConfigError(
-                f"{self.bundle_identifier!r} is not a valid bundle identifier."
-                f"\n\n"
-                "The bundle id should be a reversed domain name. It must contain at "
-                "least 2 dot-separated sections; each section may only include "
-                "letters, numbers, and hyphens; and each section may not contain any "
-                "reserved words (like 'switch', or 'while')."
+                f"{self.bundle!r} is not a valid bundle identifier.\n"
+                "\n"
+                "When the bundle is combined with an app name, it should form a "
+                "reversed domain name. The combined bundle identifier must "
+                "contain at least 2 dot-separated sections; each section may "
+                "only include letters, numbers, and hyphens; and each section "
+                "may not contain any reserved words (like 'switch', or 'while')."
             )
 
         for document_type_id, document_type in self.document_types.items():
