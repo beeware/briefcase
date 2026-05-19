5da5209ad20a93ac845b4c1ce0bb811d9eb2b60b
AJ Fisher <AJTheDataGuy@gmail.com>
2026-05-18 19:04:31 -0500

    Fixed newlines again

--- tests/integrations/flatpak/test_Flatpak__verify.py
+++ tests/integrations/flatpak/test_Flatpak__verify.py
@@ -256,7 +256,5 @@ def test_installed_unknown_builder_version(mock_tools, builder_version, capsys):
     )
 
     output = capsys.readouterr()
-    assert (
-        "WARNING: Unable to determine the version of flatpak-builder" in output.out
-    )
+    assert "WARNING: Unable to determine the version of flatpak-builder" in output.out
     assert output.err == ""
